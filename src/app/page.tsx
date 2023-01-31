dcb6d90bde2a60578000abe223a743c1d266b43f
michaelsatumba <mjsatumba@gmail.com>
2023-01-31 11:18:27 -0800

    navBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
-// import { Inter } from '@next/font/google';
-import styles from './page.module.css';
-
-// const inter = Inter({ subsets: ['latin'] });
+import Link from 'next/link';
+import Navbar from '../components/Navbar';
+import React from 'react';
 
 export default function Home() {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Navbar />
 		</div>
 	);
 }
